4f4722dad4cb3f4e479267ee8f603f3abe509961
zeroAska <trotwoodray@gmail.com>
2015-05-27 16:25:11 +0800

    rm useless xml configs. add a folder for temp config files.
    Edited by rui.a.zhang@intel.com

--- benchmarking/mod/bobject/tool/collect_interrupt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cat /proc/interrupts > $1/interrupts_begin.dat
-sleep 10
-cat /proc/interrupts > $1/interrupts_10s.dat
-sleep 10
-cat /proc/interrupts > $1/interrupts_20s.dat
-sleep 180
-cat /proc/interrupts > $1/interrupts_200s.dat
--- benchmarking/mod/bobject/tool/collect_softirq.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cat /proc/softirqs > $1/softirqs_begin.dat
-sleep 10
-cat /proc/softirqs > $1/softirqs_10s.dat
-sleep 10
-cat /proc/softirqs > $1/softirqs_20s.dat
-sleep 180
-cat /proc/softirqs > $1/softirqs_200s.dat
