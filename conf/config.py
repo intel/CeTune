297ef8ae6c73cc034ff8005e5f98676377beda09
Chendi.Xue <chendi.xue@intel.com>
2015-09-17 19:29:05 +0800

    Added two new file
    Signed-off-by: Chendi.Xue <chendi.xue@intel.com>

---
+++ conf/config.py
@@ -0,0 +1,270 @@
+import os
+import sys
+import socket
+import json
+lib_path = os.path.abspath(os.path.join('..'))
+sys.path.append(lib_path)
+from collections import OrderedDict
+from conf import common
+import re
+import argparse
+
+class TunerConfig():
+    def __init__(self, path):
+        self.tuner = common.load_yaml_conf(path) 
+        self.path = path
+        self.tuner_conf = self.format_tuner_to_all(self.tuner)
+
+    def get_group(self, request_type):
+        res = {}
+        if request_type in self.tuner_conf:
+            #for key, value in self.tuner_conf[request_type].items():
+                #res.append({"key":key,"value":value,"check":True,"dsc":""})
+            res = self.tuner_conf[request_type]
+        return res
+
+    def set_config(self, key, value, option="update"):
+        level_list = ["testjob1"]
+        level_list.extend(key.split('|'))
+        current = self.tuner
+        for level in level_list[:-1]:
+            if level not in current:
+                current[level] = OrderedDict()
+            current = current[level]
+        if option == "delete":
+            del current[level_list[-1]]
+        elif option == "update":
+            current[level_list[-1]] = value
+        with open(self.path,"w") as f:
+            f.write(json.dumps(self.tuner, indent=4))
+        return True
+
+    def group_list(self):
+        group_list = {}
+        group_list["workflow"] = ["workstages"]
+        group_list["system"] = ["version","disk"]
+        group_list["ceph_tuning"] = ["pool","global","osd","mon","client","radosgw"]
+        group_list["analyzer"] = ["analyzer"]
+        return group_list
+
+    def format_tuner_to_all(self, tuner, key=None):
+        group_list = self.group_list()
+        if not isinstance( tuner, dict ):
+            if isinstance( tuner, list ):
+                return [key, tuner]
+            else:
+                return [key, str(tuner)]
+        else:
+            output = OrderedDict()
+            for child_key in tuner:
+                find_in_group = False
+                for group in group_list:
+                    if child_key in group_list[group]:
+                        find_in_group = True
+                        if group not in output:
+                            output[group] = OrderedDict()
+                        current = output[group]
+                        new_key = child_key
+                        break
+                if not find_in_group:
+                    current = output
+                    if not key:
+                        current["jobname"] = child_key
+                        new_key = None
+                    else:
+                        new_key = "%s|%s" % (key, child_key)
+                res = self.format_tuner_to_all( tuner[child_key], new_key )
+                if isinstance(res, list):
+                    current[res[0]]=res[1]
+                else:
+                   current.update(res)
+            return output
+
+class Config():
+    def __init__(self, conf_path):
+        self.conf_data = OrderedDict()
+        self.conf_path = conf_path
+        cur_conf_section = self.conf_data
+        self.group = OrderedDict()
+        cur_group = "global"
+        self.group[cur_group] = []
+        with open(conf_path, "r") as f:
+            for line in f:
+                if re.search('^#', line):
+                    if "======" in line:
+                        cur_group_re = re.search('\w+', line)
+                        if cur_group_re:
+                            cur_group = cur_group_re.group(0)
+                        self.group[cur_group] = []
+                    continue
+                section = re.search('^\[(\w+)\]', line)
+                if section:
+                    self.conf_data[section.group(1)] = {}
+                    cur_conf_section = self.conf_data[section.group(1)]
+                else:
+                    try:
+                        key, value = line.split("=")
+                        key = key.strip()
+                        value = value.strip()
+                        if( value[-1] == '\n' ):
+                            value = value[:-1]
+                        if value != "":
+                            value = value.strip('"')
+                        self.group[cur_group].append(key)
+                        self.conf_data[key] = value
+                        if cur_conf_section != self.conf_data:
+                            cur_conf_section[key] = value
+                    except:
+                        pass
+
+    def dump_to_file(self, output, key=""):
+        line_list = []
+        group_list = self.get_group_list()
+        for request_type in group_list:
+            line_list.append("#============%s============" % request_type)
+            res = self.get_group( request_type )
+            for key, value in res.items():
+                if value == "":
+                    value = '""'
+                line_list.append("%s=%s" % (key, value))
+        #print "\n".join(line_list)
+        with open(output, 'w') as f:
+            f.write( "\n".join(line_list) )
+
+    def set_config(self, request_type, key, value, option="update"):
+        if request_type not in self.group:
+            self.group[request_type] = []
+        
+        if option == "delete":
+            del self.conf_data[key]
+            self.group[request_type].remove(key)
+        elif option == "update":
+            self.conf_data[key] = value
+            if request_type not in self.group:
+                self.group[request_type] = []
+            if key not in self.group[request_type]:
+                self.group[request_type].append(key)
+
+        self.dump_to_file(self.conf_path)
+        return True
+    
+    def get(self, key, dotry=False):
+        if key in self.conf_data:
+            return self.conf_data[key]
+        else:
+            print "%s not defined in all.conf" % key
+            if not dotry:
+                sys.exit()
+            else:
+                return ""
+
+    def get_list(self,key):
+        if key in self.conf_data:
+            if re.search(',', self.conf_data[key]):
+                return self.conf_data[key].split(",")
+            else:
+                return [self.conf_data[key]]
+        else:
+            print "%s not defined in all.conf" % key
+
+    def get_all(self):
+        return self.conf_data
+
+    def get_group(self, request_type):
+        res = OrderedDict()
+        if request_type in self.group:
+            for key in self.group[request_type]:
+                res[key] = self.get(key)
+        return res
+
+    def get_group_list(self):
+        return self.group
+
+class BenchmarkConfig():
+    def __init__(self):
+        self.conf_path = "../conf/cases.conf"
+        self.default_conf_path = "../conf/cases.default.conf"
+ 
+    def set_config(self, case_json_list):
+        testcase_keys = [
+            "benchmark_driver","worker", "container_size", "iopattern",
+            "op_size", "object_size/QD", "rampup", "runtime", "device"
+        ]
+        case_list = []
+        for tmp_dict in json.loads(case_json_list):
+            tmp = []
+            for key in testcase_keys:
+                tmp.append(tmp_dict[key])
+            if tmp not in case_list:
+                case_list.append(tmp)
+        if len(case_list):
+            with open("../conf/cases.conf","w") as f:
+                for case_items in case_list:
+                    f.write('%8s\t%4s\t%16s\t%8s\t%8s\t%16s\t%8s\t%8s\t%8s\n' % ( case_items[0],case_items[1], case_items[2], case_items[3], case_items[4], case_items[5], case_items[6], case_items[7], case_items[8] ) )
+        #run_cases.main(['--option', "gen_case"])
+        return False
+
+    def check_case_conf(self):
+        line_num = 0
+        with open("../conf/cases.conf","r") as casefile:
+            for line in casefile.readlines():
+                line_num = line_num+1
+                line_list = line.split()
+                engine,workers,volume_size,io_pattern,record_size,qd,rampup_time,run_time,disk = line_list
+
+                if engine in ["qemurbd","fiorbd"]:
+                    if engine == "qemurbd":
+                        case = "'\["+engine+"-"+io_pattern+"-"+record_size+"-"+"qd"+qd+"-"+volume_size+"-"+rampup_time+"-"+run_time+"-vdb\]'"
+                    if engine == "fiorbd":
+                        case = "'\["+engine+"-"+io_pattern+"-"+record_size+"-"+"qd"+qd+"-"+volume_size+"-"+rampup_time+"-"+run_time+"-fiorbd\]'"
+
+                if engine == "cosbench":
+                    case = workers+"-cosbench-"+io_pattern+"-"+record_size+"-100con-100obj-"+rampup_time+"-"+run_time+"-cosbench.xml"
+                       
+    def get_config(self):
+        if not (os.path.exists("../conf/cases.conf")):
+            common.bash("cp %s %s" % (self.default_conf_path, self.conf_path))
+        #self.check_case_conf()
+        testcase_list = []
+        with open(self.conf_path,"r") as f:
+            for line in f.readlines():
+                p = line.split()
+                testcase_list.append( self.parse_benchmark_cases( p ) )
+        return testcase_list
+        
+    def parse_benchmark_cases(self, testcase):
+        p = testcase
+        testcase_dict = {
+            "benchmark_driver":p[0],"worker":p[1], "container_size":p[2], "iopattern":p[3],
+            "op_size":p[4], "object_size/QD":p[5], "rampup":p[6], "runtime":p[7], "device":p[8]
+        }
+        return testcase_dict
+
+def main(args):
+    parser = argparse.ArgumentParser(description='debug')
+    parser.add_argument(
+    'operation'
+    )
+    args = parser.parse_args(args)
+    benchmarkConfig = BenchmarkConfig()
+    if args.operation == "set_config":
+        testcase_list = [{"rampup": "0", "iopattern": "seqwrite", "op_size": "64k", "object_size/QD": "64", "device": "/dev/vdb", "benchmark_driver": "qemurbd", "runtime": "100", "worker": "20", "container_size": "40g"},
+                        {"rampup": "0", "iopattern": "seqread", "op_size": "64k", "object_size/QD": "64", "device": "/dev/vdb", "benchmark_driver": "qemurbd", "runtime": "100", "worker": "20", "container_size": "40g"},
+                        {"rampup": "0", "iopattern": "randwrite", "op_size": "4k", "object_size/QD": "4", "device": "/dev/vdb", "benchmark_driver": "qemurbd", "runtime": "100", "worker": "20", "container_size": "40g"},
+                        {"rampup": "0", "iopattern": "randread", "op_size": "4k", "object_size/QD": "4", "device": "/dev/vdb", "benchmark_driver": "qemurbd", "runtime": "100", "worker": "20", "container_size": "40g"}
+                        ]
+        benchmarkConfig.set_config(testcase_list)
+    
+    if args.operation == "get_case_conf":
+        #myconfig = Config("../conf/cases.conf")
+        #myconfig.gen_case_conf()
+        get_case_conf()
+
+    if args.operation == "check_case_conf":
+        #myconfig = Config("../conf/cases.conf")
+        #myconfig.check_case_conf()
+        benchmarkConfig.check_case_conf()
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv[1:])
---
+++ conf/handler.py
@@ -0,0 +1,132 @@
+import os
+import sys
+import socket
+import json
+lib_path = os.path.abspath(os.path.join('..'))
+sys.path.append(lib_path)
+from collections import OrderedDict
+from conf import config
+import re
+import argparse
+
+class ConfigHandler():
+    def __init__(self):
+        self.all_conf = config.Config("../conf/all.conf")
+        self.tuner_conf = config.TunerConfig("../conf/tuner.yaml")
+        self.cases_conf = config.BenchmarkConfig()
+        self.required_lists = self.list_required_config()
+
+    def get_group(self, request_type):
+        res = []
+        tmp_res = OrderedDict()
+
+        all_conf = self.all_conf.get_group(request_type)
+        tmp_res.update( all_conf )
+
+        tuner_conf = self.tuner_conf.get_group(request_type)
+        tmp_res.update( tuner_conf )
+
+        if request_type == "testcase":
+            cases_conf = self.cases_conf.get_config()
+            return cases_conf
+
+        if request_type in self.required_lists:
+            for required_key in self.required_lists[request_type]:
+                 if required_key not in tmp_res:
+                     tmp_res[required_key] = self.required_lists[request_type][required_key]
+                     self.set_config(request_type, required_key, tmp_res[required_key])
+
+        for key, value in tmp_res.items():
+            res.append({"key":key,"value":value,"check":True,"dsc":""})
+
+        return res
+
+    def set_config(self, request_type, key, value):
+        conf_type = self.get_corresponde_config(request_type)
+        output = {}
+        output["key"] = key
+        output["value"] = value
+        output["dsc"] = ""
+        output["check"] = False
+        if conf_type == "tuner":
+            res = self.tuner_conf.set_config(key, value)
+        elif conf_type == "all":
+            res = self.all_conf.set_config(request_type, key, value)
+        if conf_type == "cases":
+            res = self.cases_conf.set_config(value)
+        if res:
+            output["check"] = True 
+        return output
+
+    def del_config(self, request_type, key):
+        conf_type = self.get_corresponde_config(request_type)
+        if conf_type == "tuner":
+            res = self.tuner_conf.set_config(key, "", option="delete")
+        elif conf_type == "all":
+            res = self.all_conf.set_config(request_type, key, "", option="delete")
+        return res
+
+    def get_group_config(self, request_type):
+        config_type = self.get_corresponde_conf( request_type )
+        if config_type == "tuner":
+            res = self.tuner_conf.get_group(request_type)
+        elif config_type == "all":
+            res = self.all_conf.get_group(request_type)
+
+    def get_corresponde_config(self, request_type):
+        key_to_file = {}
+        key_to_file["tuner"] = ["workflow","system","ceph_tuning","analyzer"]
+        key_to_file["all"] = ["cluster","ceph_hard_config","benchmark"]
+        key_to_file["cases"] = ["testcase"]
+        for key, value in key_to_file.items():
+            if request_type in value:
+                return key
+        return None
+
+    def check_config_correction(self):
+        pass
+
+    def check_required_config(self):
+        pass
+
+    def list_required_config(self):
+        required_list = {}
+        required_list["cluster"] = OrderedDict()
+        required_list["cluster"]["head"] = socket.gethostname()
+        required_list["cluster"]["user"] = "root"
+        required_list["cluster"]["list_server"] = ""
+        required_list["cluster"]["list_client"] = ""
+        required_list["cluster"]["list_mon"] = ""
+        required_list["cluster"]["disk_num_per_client"] = 10
+        required_list["cluster"]["rgw_server"] = ""
+        required_list["cluster"]["rgw_start_index"] = 1
+        required_list["cluster"]["rgw_num_per_server"] = 5
+        required_list["cluster"]["osd_partition_count"] = 1
+        required_list["cluster"]["osd_partition_size"] = ""
+        required_list["cluster"]["journal_partition_count"] = 5
+        required_list["cluster"]["journal_partition_size"] = "10G"
+        required_list["ceph_hard_config"] = OrderedDict()
+        required_list["ceph_hard_config"]["public_network"] = ""
+        required_list["ceph_hard_config"]["cluster_network"] = ""
+        required_list["ceph_hard_config"]["mon_data"] = "/var/lib/ceph/ceph.$id"
+        required_list["ceph_hard_config"]["osd_objectstore"] = "filestore"
+        required_list["benchmark"] = OrderedDict()
+        required_list["benchmark"]["fio_capping"] = "false"
+        required_list["benchmark"]["volume_size"] = 40960
+        required_list["benchmark"]["rbd_volume_count"] = 80
+        required_list["benchmark"]["disk_num_per_client"] = ""
+        required_list["benchmark"]["dest_dir"]="/mnt/data/"
+        required_list["benchmark"]["dest_dir_remote_bak"]="192.168.3.101:/share/chendi_new/cetune_demo"
+
+        required_list["workflow"] = OrderedDict()
+        required_list["workflow"]["workstages"] = ["deploy","benchmark"]
+        required_list["system"] = OrderedDict()
+        required_list["system"]["disk|read_ahead_kb"] = 2048
+        required_list["ceph_tuning"] = OrderedDict()
+        required_list["ceph_tuning"]["pool|rbd|size"] = 2
+        required_list["ceph_tuning"]["global|mon_pg_warn_max_per_osd"] = 1000
+        required_list["analyzer"] = OrderedDict()
+        required_list["analyzer"]["analyzer"] = "all"
+
+        return required_list
+        
