860e8eaaf7ec2eb13fb1d955ee8d9d4090e2afce
Chendi Xue <chendi.xue@intel.com>
2016-12-14 10:23:48 +0800

    Add 99.99 latency at summary page
    Signed-off-by: Chendi Xue <chendi.xue@intel.com>

--- visualizer/visualizer.py
+++ visualizer/visualizer.py
@@ -214,7 +214,7 @@ class Visualizer:
 
                 #runid = int(data[0])
                 runid = re.findall('id=(.*?)>', tr_start, re.S)[0]
-                if len(data) < 17:
+                if len(data) < 18:
                     data.insert(2, "")
                 formated_report[runid] = tr_start
                 for block in data:
@@ -290,9 +290,10 @@ class Visualizer:
         output.append(" <th data-resizable-column-id='12'><a title='Benchmarked IOPS' id='runid_fio_iops' href='#'>IOPS</a></th>")
         output.append(" <th data-resizable-column-id='13'><a title='Benchmarked Bandwidth' id='runid_fio_bw' href='#'>BW(MB/s)</a></th>")
         output.append(" <th data-resizable-column-id='14'><a title='Benchmarked Latency' id='runid_fio_latency' href='#'>Latency(ms)</a></th>")
-        output.append(" <th data-resizable-column-id='15'><a title='Storage Node IOPS' id='runid_osd_iops' href='#'>SN_IOPS</a></th>")
-        output.append(" <th data-resizable-column-id='16'><a title='Storage Node Bandwidth' id='runid_osd_bw' href='#'>SN_BW(MB/s)</a></th>")
-        output.append(" <th data-resizable-column-id='17'><a title='Storage Node Latency' id='runid_osd_latency' href='#'>SN_Latency(ms)</a></th>")
+        output.append(" <th data-resizable-column-id='15'><a title='Benchmarked Latency 99.99' id='runid_fio_latency_99' href='#'>99.99% Latency(ms)</a></th>")
+        output.append(" <th data-resizable-column-id='16'><a title='Storage Node IOPS' id='runid_osd_iops' href='#'>SN_IOPS</a></th>")
+        output.append(" <th data-resizable-column-id='17'><a title='Storage Node Bandwidth' id='runid_osd_bw' href='#'>SN_BW(MB/s)</a></th>")
+        output.append(" <th data-resizable-column-id='18'><a title='Storage Node Latency' id='runid_osd_latency' href='#'>SN_Latency(ms)</a></th>")
         output.append(" </tr>")
         return output
 
