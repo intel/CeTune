e3d4e2d7da1feef40893304b06a9ac93fb584237
Chendi.Xue <chendi.xue@intel.com>
2017-01-09 10:18:45 +0800

    Update generic.py
    align to current webui updated testcase.

--- benchmarking/mod/generic/generic.py
+++ benchmarking/mod/generic/generic.py
@@ -205,8 +205,8 @@ class Generic(Benchmark):
             "block_size":p[3], "qd":p[4], "rampup":p[5], 
             "runtime":p[6], "vdisk":p[7]
         }
-        if len(p) >= 9:
-            testcase_dict["description"] = p[8]
+        if len(p) >= 10:
+            testcase_dict["description"] = p[9]
         else:
             testcase_dict["description"] = ""
         return testcase_dict
